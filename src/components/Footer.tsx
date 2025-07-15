967817f92b1052d9cf0e05d03bbf31ba78f8ed25
coxlaurel <coxlaurel@gmail.com>
2025-07-15 17:32:10 -0400

    images

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,7 @@
 function Footer() {
     return <>
         <div className="footer-container" style={{marginBottom: "10vh"}}>
-            <p>Site coded from scratch by me using React, TypeScript, CSS, and gsap for scrolling sections. Hosted on Vercel. Inspired heavily by <a href="https://brittanychiang.com/#about">Brittany Chiang's beautiful website</a>. Thanks for stopping by! &hearts;</p>
+            <p>Site coded from scratch by me using React, TypeScript, CSS, and gsap for scrolling sections. Hosted on GitHub Pages. Inspired heavily by <a href="https://brittanychiang.com/#about">Brittany Chiang's beautiful website</a>. Thanks for stopping by! &hearts;</p>
         </div>
     </>
 }

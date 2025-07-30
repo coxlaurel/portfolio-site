6fdf5585951138d5b55cd456bd1437636e29622d
coxlaurel <coxlaurel@gmail.com>
2025-07-30 16:47:22 -0400

    link update

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -4,7 +4,7 @@ function About() {
     return <>
         <div className='about-content'>
             <p>Hi! My name is Laurel Cox and I’m a junior at Olin College of Engineering studying electrical and computer engineering. I am excited about leveraging software and electrical integration technologies to build a more environmentally sustainable future.</p>
-            <p>Currently, I’m interning at TTM Technologies in their Automation and Robotics division. In this role, I spend most of my day rebuilding the Industry 4.0 SCADA system to have a Unified Namespace (UNS) architecture and delivering new features to the existing 4.0 system every sprint cycle. Additionally, I’m learning a lot about PCB manufacturing and getting to work on machines built in-house. </p>
+            <p>Currently, I’m interning at TTM Technologies in their Automation and Robotics division. In this role, I spend most of my day rebuilding the Industry 4.0 SCADA system to have a Unified Namespace (UNS) architecture and delivering new features to end users on the existing 4.0 system. Additionally, I’m learning a lot about PCB manufacturing and getting hands on work fabricating parts for machines built in-house. </p>
             <p>Outside of engineering, I’m interested in entrepreneurship and am on the board for Olin’s entrepreneurship club, Catalyst. In leadership roles, like Catalyst and the Society of Women Engineers (SWE), I found a love for community building and creating environments that lift everyone.</p>
             <p>In my free time, you can catch me knitting, reading, listening to She’s Green, or getting whisked off to an adventure with my friends.</p>        
         </div>
